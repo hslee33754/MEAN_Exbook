3a9ca97234e1dec48cadc833f5af15bb4abc9e44
Kate <hslee.33754@gmail.com>
2016-07-16 08:16:41 +0000

    install express and set express app

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var http = require('http');
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
 
 http.createServer(function(req,res){
     res.writeHead(200, {'Content-Type' : 'text/plain'});
     res.end('Hello World');
-}).listen(process.env.PORT || 3000);
+}).listen(app.get('port') || 3000);
 
-console.log('Server strted on localhost:' + process.env.PORT + '; press Ctrl-C to terminate.');
+console.log('Server strted on localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
